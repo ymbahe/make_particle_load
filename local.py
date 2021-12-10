b3feba7eb17fa4513d2eafbcd9a53dbe9d3f1310
dc-bahe1@cosma.dur.ac.uk <bahe@strw.leidenuniv.nl>
2021-12-10 09:05:21 +0000

    Updated local.py

---
+++ local.py
@@ -0,0 +1,6 @@
+local = {
+    'slurm_partition': 'ENTER_SLURM_PARTITION_HERE',
+    'slurm_account': 'ENTER_SLURM_ACCOUNT_HERE',
+    'slurm_email': 'ENTER_EMAIL_ADDRESS_HERE',
+    'cpus_per_node': ENTER_CPUS_PER_NODE_HERE,
+}
