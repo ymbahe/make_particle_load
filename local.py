013e34dc1b48e038170b68b8438fc7e1ff192058
dc-bahe1@cosma.dur.ac.uk <bahe@strw.leidenuniv.nl>
2021-12-10 09:03:04 +0000

    Removed local.py from index

--- local.py
+++
@@ -1,6 +0,0 @@
-local = {
-    'slurm_partition': 'ENTER_SLURM_PARTITION_HERE',
-    'slurm_account': 'ENTER_SLURM_ACCOUNT_HERE',
-    'slurm_email': 'ENTER_SLURM_EMAIL_HERE',
-    'cpus_per_node': 'ENTER_NUMBER_OF_CORES_PER_NODE_HERE',
-}
