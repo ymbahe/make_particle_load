a51ce44730d4162902c9710bc7825405efadf81b
Yannick Bahe <bahe@strw.leidenuniv.nl>
2022-07-15 21:09:05 +0200

    First unit test added

---
+++ tests/README.rst
@@ -0,0 +1,7 @@
+=====
+Tests
+=====
+
+This directory contains a suite of tests that are intended to verify the
+correct functioning of `zoom-setup`. They can be executed automatically
+with `pytest`.
---
+++ tests/test_mask.py
@@ -0,0 +1,23 @@
+"""Test suite for make_mask.py module"""
+
+import numpy as np
+
+from make_mask.make_mask import find_vertices
+
+
+def test_vertices():
+    r = np.array([
+        [0.1, 4, 2.1],
+        [-3.2, 5.5, 1.0],
+        [9.0, 2.1, -3.3],
+        [0.5, 4.1, 3.0]
+    ])
+    box = find_vertices(r, serial_only=True)
+
+    np.testing.assert_allclose(
+        box, np.array([[-3.2, 2.1, -3.3], [9.0, 5.5, 3.0]]))
+    
+    return
+
+
+
