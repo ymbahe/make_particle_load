1da9051340332649396b679b9fb8d3729018c757
Yannick Bahe <bahe@strw.leidenuniv.nl>
2022-07-21 13:33:02 +0200

    Properly use settings from local.py in GPL

--- local.py
+++ local.py
@@ -2,5 +2,6 @@ local = {
     'slurm_partition': 'ENTER_SLURM_PARTITION_HERE',
     'slurm_account': 'ENTER_SLURM_ACCOUNT_HERE',
     'slurm_email': 'ENTER_EMAIL_ADDRESS_HERE',
-    'cpus_per_node': ENTER_CPUS_PER_NODE_HERE,
+    'cpus_per_node': 'ENTER_CPUS_PER_NODE_HERE',
+    'memory_per_core': 'ENTER_MEMORY_PER_CORE_HERE',
 }
