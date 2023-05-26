1e71e5414f5b5bf947cd05ae7200c2c7805126ff
dc-bahe1@cosma.dur.ac.uk <bahe@strw.leidenuniv.nl>
2023-05-26 20:27:19 +0100

    Updated SWIFT template files

--- swift_templates/postprocess.sh
+++ swift_templates/postprocess.sh
@@ -5,8 +5,15 @@
 snap_basename=$1
 snap_num=$2
 
-cp run_vr_template.sh run_vr_$snap_num.sh
-sed -i "s/XXX-SNAP-STRING-XXX/${snap_num}/g" run_vr_$snap_num.sh
-sed -i "s/XXX-SNAP-BASENAME-XXX/${snap_basename}/g" run_vr_$snap_num.sh
+token=`h5dump -a Header/SelectOutput ${snap_basename}_${snap_num}.hdf5 | grep "Snapshot"`
 
-sbatch < run_vr_$snap_num.sh
+if [[ $token == "" ]]; then
+    echo "Not a snapshot, skipping"
+else
+
+    cp run_vr_template.sh run_vr_$snap_num.sh
+    sed -i "s/XXX-SNAP-STRING-XXX/${snap_num}/g" run_vr_$snap_num.sh
+    sed -i "s/XXX-SNAP-BASENAME-XXX/${snap_basename}/g" run_vr_$snap_num.sh
+    
+    sbatch < run_vr_$snap_num.sh
+fi
