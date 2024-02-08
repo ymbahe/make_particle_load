5ee287bc7e175e67a049b47a303bae89324d133c
Kyle Oman <kyle.a.oman@durham.ac.uk>
2024-02-08 14:42:54 +0000

    Replace tabs with spaces.

--- swift_setup.py
+++ swift_setup.py
@@ -207,8 +207,8 @@ def get_ic_metadata(args):
         data['m_pt1'] = f['PartType1']['Masses'][0] / data['HubbleParam']
         print(f"Read PT1 mass as {data['m_pt1']}")
 
-	data['m_pt0'] = f['PartType0']['Masses'][0] / data['HubbleParam']
-	print(f"Read gas (PT0) mass as {data['m_gas']}"
+        data['m_pt0'] = f['PartType0']['Masses'][0] / data['HubbleParam']
+        print(f"Read gas (PT0) mass as {data['m_gas']}"
 
         data['is_zoom'] = h.attrs['NumPart_Total'][2] > 0
 
